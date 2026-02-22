b66f6572aa0b356898d72487411aff971715128a
Parham <arianezhadparham@gmail.com>
2026-02-22 13:19:22 +0330

    feat: Add header

---
+++ free-shipping-progress.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Free Shipping Progress
+ * Plugin URI: https://github.com/Parham-Arianezhad/free-shipping-progress.git
+ * Description: Show remaining amount to reach free shipping.
+ * Version: 1.0.0
+ * Author: Parham Arianezhad
+ * Author URI: https://www.linkedin.com/in/parham-arianezhad/
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: free-shipping-progress
+ * Domain Path: /languages
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Plugin constants
+define('FSP_PATH', plugin_dir_path(__FILE__));
+define('FSP_URL', plugin_dir_url(__FILE__));
+define('FSP_VERSION', '1.0.0');
